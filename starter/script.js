9c3a8bcfa61a6c2ae0eaabbe888ad09e521e67d2
Lily Halladay <lilyhalladay14@gmail.com>
2023-01-08 14:26:07 +0000

    added while statements so questions loop of user chooses invalid input

--- starter/script.js
+++ starter/script.js
@@ -94,20 +94,39 @@ getPasswordOptions ()
 // Function to prompt user for password options
 function getPasswordOptions() {
 let confirmLength = (prompt("How many characters would you like your password to contain?"));
-if (confirmLength <= 9 || confirmLength >= 64) {
+while (confirmLength <= 9 || confirmLength >= 64) {
   alert("Password length must be between 10-64 characters, please try again");
+  confirmLength = (prompt("How many characters would you like your password to contain?"));
 }
+alert("Thank you. Your password length will be " + confirmLength + " characters"); 
+
 let confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
 let confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
 let confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
 let confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
-if(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
-  alert("Please choose at least one character type")}
+
+while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+  alert("Please choose at least one character type")
+  let confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+  let confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+  let confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+  let confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+}
+  
 }
 
-// Store choices in new array 
+//find a way to loop back through the options if they haven't chosen any character types 
 
-let characterOptionsArray = []
+
+// Function for getting a random element from an array
+
+function getRandom(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+
+// Function to generate password with user input
+function generatePassword() {
+  let characterOptionsArray = []
 
 if (getPasswordOptions.confirmSpecialCharacter = confirm) {
   characterOptionsArray+getPasswordOptions.confirmSpecialCharacter.concat();
@@ -128,17 +147,6 @@ else if (getPasswordOptions.confirmUpperCase = confirm) {
 else {
   alert("You must have at least 1 type")
 }
-
-
-// Function for getting a random element from an array
-
-function getRandom(arr) {
-  return Math.floor(Math.random() * arr.length);
-}
-
-// Function to generate password with user input
-function generatePassword() {
-
 }
 
 // Get references to the #generate element
