c56eb86aba3708850f18caf4c1b0a6b0b30b259a
Lily Halladay <lilyhalladay14@gmail.com>
2023-01-09 22:06:36 +0000

    added descriptive comments and changed generate password function to be more clear

--- starter/script.js
+++ starter/script.js
@@ -90,75 +90,88 @@ let upperCasedCharacters = [
 
 let userOptions = {};
 
-// FUNCTION TO CHOOSE LENGTH OF PW
-  function getPasswordLength () {
-    let numOfChar = Number(
-      prompt("How many characters would you like in your password? Please choose a value between 10-64)")
-    );
-    if (numOfChar >= 10 && numOfChar <= 64) {
-      return numOfChar;
-    } else {
-      alert(
-        "Sorry, your password requires a minimum of 10 characters and a maximum of 64. Please try again."
-      );
-      return getPasswordLength();
-    }
-  };
-
-// FUNCTION TO CHOOSE CHARACTER OPTIONS 
-  function characterOptions () {
-    let char = {
-      confirmLowerCase: confirm("Click OK to confirm if you would like to include lowercase characters"),
-      confirmUpperCase: confirm("Click OK to confirm if you would like to include uppercase characters"),
-      confirmNumericCharacter: confirm("Click OK to confirm if you would like to include numeric characters"),
-      confirmSpecialCharacter: confirm("Click OK to confirm if you would like to include special characters"),
-    };
-    if ((char.lowerCasedCharacters === false) && (char.upperCasedCharacters === false) && (char.specialCharacters === false) && (char.numericCharacters === false)) {
-    return char;
+/* FUNCTION TO CHOOSE LENGTH OF PW
+- convert user input to number 
+- if the number inputted is within parameters then return numOfChar
+- if the number inputted is outside parameters then alert the user to try again and restart the function
+*/
+function getPasswordLength() {
+  let numOfChar = Number(
+    prompt("How many characters would you like in your password? Please choose a value between 10-64)")
+  );
+  if (numOfChar >= 10 && numOfChar <= 64) {
+    return numOfChar;
   } else {
+    alert(
+      "Sorry, your password requires a minimum of 10 characters and a maximum of 64. Please try again."
+    );
+    return getPasswordLength();
+  }
+};
+
+
+/*FUNCTION TO CHOOSE CHARACTER OPTIONS 
+- make an object which holds all the booleans for lower,upper, special and numeric cases
+- if they are all false or 'cancel' then alert to 'please select at least one option' and restart the function
+- otherwise, the user has performed the task correctly and we return char 
+*/
+function characterOptions() {
+  let char = {
+    confirmLowerCase: confirm("Click OK to confirm if you would like to include lowercase characters"),
+    confirmUpperCase: confirm("Click OK to confirm if you would like to include uppercase characters"),
+    confirmNumericCharacter: confirm("Click OK to confirm if you would like to include numeric characters"),
+    confirmSpecialCharacter: confirm("Click OK to confirm if you would like to include special characters"),
+  };
+  if ((char.confirmLowerCase === false) && (char.confirmUpperCase === false) && (char.confirmSpecialCharacter === false) && (char.confirmNumericCharacter === false)) {
     alert("Please select at least one character option!");
     return characterOptions();
+  } else {
+    return char;
   }
-  };
+};
 
-//FUNCTION TO STORE PASSWORD OPTIONS 
+/*FUNCTION TO STORE PASSWORD OPTIONS 
+- 
+*/
 function storeUserOptions() {
   userOptions = {
     numOfChar: getPasswordLength(),
     userCharacters: characterOptions(),
-  }}
+  }
+  console.log(userOptions.numOfChar);
+  console.log(userOptions.userCharacters)
+}
+
 
 //FUNCTION FOR GETTING RANDOM ELEMENT FROM ARRAY
-
 function getRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-//Function for storing random characters in userPassArr variable based on user choices
-function constructPassword() {
+//USE THE GET RANDOM FUNCTION 
+let randomSpecial = getRandom(specialCharacters);
+let randomNumeric = getRandom(numericCharacters);
+let randomLowerCase = getRandom(lowerCasedCharacters);
+let randomUpperCase = getRandom(upperCasedCharacters);
 
-  if (getPasswordOptions.confirmLowerCase == true) {
-    userPassArr += getRandom(lowerCasedCharacters);
-  }
-  if (getPasswordOptions.confirmUpperCase == true) {
-    userPassArr += getRandom(upperCasedCharacters);
-  }
-  if (getPasswordOptions.confirmNumericCharacter == true) {
-    userPassArr += getRandom(numericCharacters);
-  }
-  if (getPasswordOptions.confirmSpecialCharacter == true) {
-    userPassArr += getRandom(specialCharacters);
-  }
-}
-constructPassword()
-
-//Function to make sure the password is the appropriate length based on user input 
+//FUNCTION TO GENERATE PASSWORD 
 function generatePassword() {
-  while (userPassArr.length < passwordLength) {
-    constructPassword();
+  storeUserOptions()
+  let userChar = userOptions.userCharacters;
+  let charArray = [];
+
+  for (let i = 0; i <userOptions.numOfChar; i++) {
+    if(userChar.confirmLowerCase){
+      charArray.push(getRandom(lowerCasedCharacters))
+    }if(userChar.confirmUppercase){
+      charArray.push(getRandom(upperCasedCharacters))
+    }if(userChar.confirmNumericCharacter){
+      charArray.push(getRandom(numericCharacters))
+    }if(userChar.confirmSpecialCharacter){
+      charArray.push(getRandom(specialCharacters))
+    }
   }
 }
-generatePassword();
 
 // Get references to the #generate element
 let generateBtn = document.querySelector('#generate');
@@ -172,4 +185,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword); */
+generateBtn.addEventListener('click', writePassword); 
